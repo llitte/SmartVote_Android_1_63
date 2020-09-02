576afc41e3d0e49ed598882c78b5126a0226a29d
Atsadawut Mongkhonsin <atsada4188@gmail.com>
2020-09-02 14:54:58 +0700

    //add layout check permissions for vote //you can check pemissions for vote by push "NEXT" button and enter ID number card and push "ตรวจสอบสิทธิเลทอกตั้ง" button.

--- app/src/main/java/mongkhonsin/atsadawut/smartvote/PermissionsActivity.java
+++ app/src/main/java/mongkhonsin/atsadawut/smartvote/PermissionsActivity.java
@@ -1,34 +1,50 @@
 package mongkhonsin.atsadawut.smartvote;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class PermissionsActivity extends AppCompatActivity {
     private Button permissionsButton;
     private EditText editTextId;
     private String id;
-    //private Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permissions);
 
-       // intent = getIntent();
         permissionsButton = findViewById(R.id.permissions_button);
-        editTextId = findViewById(R.id.edit_text_id);
-        id = editTextId.getText().toString();
         permissionsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                editTextId = findViewById(R.id.edit_text_id);
+                id = editTextId.getText().toString();
+                AlertDialog.Builder dialog = new AlertDialog.Builder(PermissionsActivity.this);
+                dialog.setTitle("ผลการตรวจสอบสิทธิเลือกตั้ง");
+                boolean idNumEqual13 = (id.length() == 13);
+                boolean idEqual_1 = (id.equals("1111111111111"));
+                boolean idEqual_2 = (id.equals("2222222222222"));
+                if(!idNumEqual13){
+                    Toast toast = Toast.makeText(PermissionsActivity.this, "กรุณากรองเลขบัตรประชาชน 13 หลัก", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                else if(idNumEqual13 && idEqual_1 || idEqual_2){
+                    dialog.setMessage("คุณเป็นผู้มีสิทธิเลือกตั้ง");
+                    dialog.setNegativeButton("OK", null);
+                    dialog.show();
+                }
+                else if(idNumEqual13 && !idEqual_1 || !idEqual_2){
+                    dialog.setMessage("คุณไม่มีสิทธิเลือกตั้ง");
+                    dialog.setNegativeButton("OK", null);
+                    dialog.show();
+                }
             }
         });
-
     }
 }
